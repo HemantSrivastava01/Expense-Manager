1424d3d0bb1666bdfbc87a35fce57dc52be306ec
Hemant <srihemant@outlook.com>
2021-05-22 11:24:47 +0530

    creating custom adaptive widget

--- lib/widgets/new_txn.dart
+++ lib/widgets/new_txn.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import '../widgets/adaptive_flat_btn.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -94,26 +95,7 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
-                    Platform.isIOS
-                        ? CupertinoButton(
-                            onPressed: _presentDatePicker,
-                            child: Text(
-                              'Choose Date!',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          )
-                        : FlatButton(
-                            textColor: Colors.blueAccent,
-                            onPressed: _presentDatePicker,
-                            child: Text(
-                              'Choose Date!',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
+                    AdaptiveFlatButton('Choose Date', _presentDatePicker)
                   ],
                 ),
               ),
